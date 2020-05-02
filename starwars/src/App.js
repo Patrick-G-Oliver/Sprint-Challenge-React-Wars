e464aaf7038466a709c725e96ea7e43e4061ebde
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-05-01 20:23:39 -0400

    Character component extant, data received

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character.js';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [swapiData, setSwapiData] = useState([])
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect( () => {
+    axios.get("https://swapi.py4e.com/api/people/") 
+      .then(response => {
+        console.log('response', response.data)
+        setSwapiData(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div>
+      <div className="App">
+        <h1 className="Header">Characters</h1>
+      </div>
+
+      <Character
+        swapiData = {swapiData}
+      />
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const CharacterStylerDiv = styled.div`
+    color: green;
+    font-size: 2rem;
+    padding: 2%;
+`;
+
+const Character = props => {
+    return (
+        <CharacterStylerDiv>
+            <p>Is this working?</p>
+        </CharacterStylerDiv>
+    );
+};
+
+export default Character;
+
+/* 
+<CharacterStylerDiv>
+        <h1>{props.swapiData.name}</h1>
+        </CharacterStylerDiv>
+*/
